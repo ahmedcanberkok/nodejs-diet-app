ccf9055b8e1b8f426e23e9b004233e72c02179ad
Ahmed Canberk Ok <canberk.ok@gmail.com>
2024-10-09 18:15:22 +0300

    first commit. Project created. User and Profile structure developed

--- dietapp-backend/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // Express instance app.js'ten geliyor
-const { PORT } = require("./config/config"); // Port bilgisi config'den geliyor
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
-});
